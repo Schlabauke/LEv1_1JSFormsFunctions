3dea35d42ff9a1af05756eda1f73984ad9863534
Schlabauke <annabelle.jeffre@gmail.com>
2021-10-28 08:00:34 +0200

    Lev1_1FormsFunctions

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+let number = document.getElementById("number")
+let summe = document.getElementById("summe")
+function getMath() {
+    console.log("Fährt");
+    sum = number.value * 2;
+    summe.innerHTML = (sum);
+}
+
